8e80f25603942487b19281b33fd097f7fe1d01f1
Théo Laperrouse <theo.laperrouse@laposte.net>
2022-11-24 13:59:29 +0100

    Link grafana and mysql

--- osrm.py
+++ osrm.py
@@ -4,17 +4,15 @@ import time
 import requests
 import mysql.connector
 
-ADD_ROUTE_QUERY = "INSERT INTO `app_db`.`routes`" \
-    "(date, departure, arrival, duration)" \
-    "VALUES (%s,%s,%s,%s)"
 ROUTE_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS `app_db`.`routes` ("\
-                    "`date` INT NOT NULL ,"\
-                    "`departure` INT NOT NULL ,"\
-                    "`arrival` INT NOT NULL ,"\
-                    "`duration` INT NOT NULL ) ENGINE = InnoDB;"
+                    "`date` VARCHAR(40) NOT NULL ,"\
+                    "`departure` VARCHAR(40) NOT NULL ,"\
+                    "`arrival` VARCHAR(40) NOT NULL ,"\
+                    "`duration` DECIMAL(5) NOT NULL ) ENGINE = InnoDB;"
 
 
-def insert_route_infos(cursor, cities_data):
+def insert_route_infos(db_connex, cities_data):
+    cursor = db_connexion.cursor()
     interest_points = cities_data['interest_points']
     cities = cities_data['cities']
     for interest_point_key in interest_points.keys():
@@ -25,10 +23,13 @@ def insert_route_infos(cursor, cities_data):
                 'http://router.project-osrm.org/route/v1/car/'
                 f'{lat},{long};{lat_city},{long_city}',
                 timeout=10).json()
-            now = datetime.now().date()
+            now = datetime.now().isoformat()
             duration = round(route_data["routes"][0]["duration"]/60, 2)
-            route_data = (now, city_key, interest_point_key, duration)
-            cursor.execute(ADD_ROUTE_QUERY, route_data)
+            add_route_query = 'INSERT INTO `app_db`.`routes`' \
+                        '(date, departure, arrival, duration)' \
+                        f' VALUES ("{now}", "{city_key}", "{interest_point_key}", "{duration}")'
+            cursor.execute(add_route_query)
+    db_connex.commit()
 
 
 if __name__ == "__main__":
@@ -43,5 +44,5 @@ if __name__ == "__main__":
     with open('cities.json', 'r', encoding='utf8') as f:
         cities_json = json.load(f)
     while True:
-        insert_route_infos(database_cursor, cities_json)
-        time.sleep(60)
+        insert_route_infos(db_connexion, cities_json)
+        time.sleep(3600)
